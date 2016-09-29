383535a53a8732619a9c1039bd29095ed3d43a33
dkiriljuk <dkirilju@itcollege.ee>
2016-09-29 19:28:46 +0300

    First Program

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package com.company;
+
+/**
+ * Created by dmitri.kiriljuk on 29.09.2016.
+ */
+public class HelloWorld {
+    public static void main(String args[]) {
+        System.out.println("Hello World!");
+    }
+}
